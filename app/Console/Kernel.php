e4edfbaa1c492ccb133d2644fe2cdd195f974b51
juanrjarolin <juanjarolin994@gmail.com>
2022-04-05 21:57:54 -0400

    second commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('registered:users')->everyMinute();
+        $schedule->command('registered:users')->daily();
     }
 
     /**
